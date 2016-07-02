6dae9aa42cbad281689dfa51026dbe675af772b7
Magnus Jerre <magnus.jerre@gmail.com>
2016-07-02 10:52:11 +0200

    Implementing start game button

--- FlippaChippa/Assets/Scripts/MenuController.cs
+++ FlippaChippa/Assets/Scripts/MenuController.cs
@@ -11,15 +11,12 @@ public class MenuController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		courses = manager.GetCourseSummaries ();
-		highlightedCourse = courses [0];
+		//courses = manager.GetCourseSummaries ();
+		//highlightedCourse = courses [0];
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonUp (0)) {
-			ApplicationModel.courseLoadedId = highlightedCourse.id;
-			SceneManager.LoadScene (1, LoadSceneMode.Single);
-		}
+
+	public void StartGame(string game) {	//game will can an identifier for the gametype or specific course. Most likely a json obejct
+		//ApplicationModel.courseLoadedId = highlightedCourse.id;
+		SceneManager.LoadScene (1, LoadSceneMode.Single);
 	}
 }
