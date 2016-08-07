872f4b8940f365afeedf51a966cae13a7c53c8fb
Magnus Jerre <magnus.jerre@ciber.com>
2016-08-07 15:28:53 +0200

    Making sure that the target stack can't consist of only on chip prefab id value

--- FlippaChippa/Assets/Scripts/Utils/CourseMetaGenerator.cs
+++ FlippaChippa/Assets/Scripts/Utils/CourseMetaGenerator.cs
@@ -3,17 +3,14 @@
 public class CourseMetaGenerator
 {
 	public static CourseMeta Generate(PrefabsManager manager) {
-		int size = Random.Range (4, 15);
+		int size = Random.Range (2, 4);
 		int flips = Random.Range (size, (int) (size * 1.5f));
 		int flips2 = Random.Range (size, (int) (size * 1.5f));
 		return Generate (size, flips, flips2, manager);
 	}
 
 	public static CourseMeta Generate(int size, int flips, int flips2, PrefabsManager manager) {
-		int[] chipIds = new int[size];
-		for (int i = 0; i < size; i++) {
-			chipIds [i] = manager.GetRandomChipId ();
-		}
+		int[] chipIds = GenerateNonIdenticalChipIDs (size, manager);
 		int[] startFlips = new int[flips];
 		for (int i = 0; i < size; i++) {
 			startFlips [i] = Random.Range (0, size);
@@ -24,5 +21,46 @@ public class CourseMetaGenerator
 		}
 		CourseMeta meta = new CourseMeta (chipIds, startFlips, targetFlips);
 		return meta;
+	}
+
+	/**
+	 * Generates a list of IDs guaranteeing that at least two different IDs exist as long
+	 * as the prefabs manager contains at least two different IDs
+	 */ 
+	private static int[] GenerateNonIdenticalChipIDs(int size, PrefabsManager manager) {
+		if (manager.chipPrefabs.Length < 2) {
+			throw new UnityException ("Not enough chip prefabs to generate a non-identical stack");
+		}
+
+		int[] chipIDs = GenerateChipIDs (size, manager);
+		while (AllValuesAreTheSame (chipIDs)) {
+			chipIDs = GenerateChipIDs (size, manager);
+		}
+		return chipIDs;
+	}
+
+	private static int[] GenerateChipIDs(int size, PrefabsManager manager) {
+		int[] chipIDs = new int[size];
+		for (int i = 0; i < size; i++) {
+			chipIDs [i] = manager.GetRandomChipId ();
+		}
+		return chipIDs;
+	}
+
+	/**
+	 * Returns false if the list doesn't have any elements or is null
+	 */ 
+	private static bool AllValuesAreTheSame(int[] array) {
+		if (array == null || array.Length == 0) {
+			return false;
+		}
+
+		int value = array [0];
+		for (int i = 0; i < array.Length; i++) {
+			if (value != array [i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
