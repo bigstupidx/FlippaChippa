b334993402cae855906cc9ac106c9578dba4067c
Magnus Jerre <magnus.jerre@ciber.com>
2016-08-31 23:43:34 +0200

    Displaying time and target checks for statistics menu

--- FlippaChippa/Assets/Scripts/Controllers/StatsMenuController.cs
+++ FlippaChippa/Assets/Scripts/Controllers/StatsMenuController.cs
@@ -7,15 +7,122 @@ public class StatsMenuController : MonoBehaviour
 {
 
 	public Text totalGamesPlayed, totalFlips, flipsPerGame;
+	public Text totalChecks, checksPerGame;
+	public Text totalTime, timePerGame;
+	public Text gamesPlayed1, gamesPlayed2;
+	public Button NextButton, PreviousButton;
+	public RectTransform flipsContent, targetContent, timeContent, allContent;
+	private int currentVisible = 0;	//0 == FlipsContent
+	public RectTransform canvasRect;
+	private float startPos = 0f, endPos = 0f;
+	private float elapsedTime = 0f;
+	public float animationTime = 0.5f;
+	private bool doMove = false;
+
+	void Awake() {
+		SetSwipeButtonsEnable ();
+	}
 
 	void Start() {
 		totalGamesPlayed.text = "" + ApplicationModel.statistics.TotalGamesPlayed ();
 		totalFlips.text = "" + ApplicationModel.statistics.TotalFlips ();
 		flipsPerGame.text = "" + (int)(ApplicationModel.statistics.CalcAverageFlipsPerGame ());
+		totalChecks.text = "" + ApplicationModel.statistics.TotalTargetChecks ();
+		checksPerGame.text = "" + ApplicationModel.statistics.TargetChecksPerGame ();
+		totalTime.text = getTimeString (ApplicationModel.statistics.TotalTime ());
+		timePerGame.text = getTimeString (ApplicationModel.statistics.TimePerGame ());
+
+		Debug.Log ("width: " + canvasRect.rect.width);
+
+		targetContent.anchoredPosition = new Vector2 (canvasRect.rect.width, targetContent.anchoredPosition.y);
+		timeContent.anchoredPosition = new Vector2 (canvasRect.rect.width * 2, timeContent.anchoredPosition.y);
+	}
+
+	void Update() {
+		if (doMove) {
+			elapsedTime += Time.deltaTime;
+			if (elapsedTime >= animationTime) {
+				doMove = false;
+				allContent.anchoredPosition = new Vector2 (endPos, allContent.anchoredPosition.y);
+			} else {
+				float progress = elapsedTime / animationTime;
+				float newPos = startPos + (endPos - startPos) * progress;
+				allContent.anchoredPosition = new Vector2 (newPos, allContent.anchoredPosition.y);
+			}
+		}
+	}
+
+	public void NextScreen() {
+		if (currentVisible == 2) {	//Showing the last screen
+			return;
+		}
+		startPos = -currentVisible * canvasRect.rect.width;
+		endPos = startPos - canvasRect.rect.width;
+		doMove = true;
+		elapsedTime = 0f;
+		currentVisible++;
+		SetSwipeButtonsEnable ();
+	}
+
+	public void PreviousScreen() {
+		if (currentVisible == 0) {	//Showing the first screen
+			return;
+		}
+
+		startPos = -currentVisible * canvasRect.rect.width;
+		endPos = startPos + canvasRect.rect.width;
+		doMove = true;
+		elapsedTime = 0f;
+		currentVisible--;
+		SetSwipeButtonsEnable ();
 	}
 
 	public void Menu() {
 		SceneManager.LoadScene (0, LoadSceneMode.Single);
+	}
+
+	private void SetSwipeButtonsEnable() {
+		if (currentVisible == 0) {	//Showing the target stack
+			DisableLeftButton ();
+		} else {
+			EnableLeftButton ();
+		}
+
+		if (currentVisible == 2) {
+			DisableRightButton ();
+		} else {
+			EnableRightButton ();
+		}
+	}
+
+	public void DisableLeftButton() {
+		PreviousButton.interactable = false;
+	}
+
+	public void DisableRightButton() {
+		NextButton.interactable = false;
+	}
+
+	public void EnableLeftButton() {
+		PreviousButton.interactable = true;
+	}
+
+	public void EnableRightButton() {
+		NextButton.interactable = true;
+	}
+
+	string getTimeString(float time) {
+		int timeInSeconds = (int)time;
+		int seconds = timeInSeconds % 60;
+		int minutes = (timeInSeconds - seconds) / 60;
+		int hours = timeInSeconds / (60 * 60);
+		string secondsString = seconds < 10 ? "0" + seconds : "" + seconds;
+		string minutesString = minutes < 10 ? "0" + minutes : "" + minutes;
+		string hoursString = hours < 10 ? "0" + hours + ":" : "" + hours + ":";
+		if (hours == 0) {
+			hoursString = "";
+		}
+		return hoursString + minutesString + ":" + secondsString;
 	}
 
 }
