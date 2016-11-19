a3ab1e19d209273464c2e4d9d10116d2ecab203d
Magnus Jerre <magnus.jerre@gmail.com>
2016-11-19 23:46:09 +0100

    Including crushweight in the help-screen

---
+++ FlippaChippa/Assets/Scripts/Controllers/Switcher.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Switcher : MonoBehaviour {
+
+	public RectTransform[] screens;
+	public Button NextButton, PreviousButton;
+	public RectTransform canvasRect, allContent;
+
+	private int currentVisible = 0, lastVisible = 0;	//0 == FlipsContent
+	private float startPos = 0f, endPos = 0f;
+	private float elapsedTime = 0f;
+	public float animationTime = 0.5f;
+	private bool doMove = false;
+
+	// Use this for initialization
+	void Start () {
+		lastVisible = screens.Length - 1;
+
+		for (int i = 0; i < screens.Length; i++) {
+			RectTransform rect = screens [i];
+			rect.anchoredPosition = new Vector2 (i * canvasRect.rect.width, rect.anchoredPosition.y);
+		}
+
+		SetSwipeButtonsEnable ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doMove) {
+			elapsedTime += Time.deltaTime;
+			if (elapsedTime >= animationTime) {
+				doMove = false;
+				allContent.anchoredPosition = new Vector2 (endPos, allContent.anchoredPosition.y);
+			} else {
+				float progress = elapsedTime / animationTime;
+				float lerp = MJMath.LerpPyramid (progress);
+				float newPos = startPos + (endPos - startPos) * lerp;
+				allContent.anchoredPosition = new Vector2 (newPos, allContent.anchoredPosition.y);
+			}
+		}	
+	}
+
+
+	public void NextScreen() {
+		if (currentVisible == lastVisible) {	//Showing the last screen
+			return;
+		}
+		startPos = -currentVisible * canvasRect.rect.width;
+		endPos = startPos - canvasRect.rect.width;
+		doMove = true;
+		elapsedTime = 0f;
+		currentVisible++;
+		SetSwipeButtonsEnable ();
+	}
+
+	public void PreviousScreen() {
+		if (currentVisible == 0) {	//Showing the first screen
+			return;
+		}
+
+		startPos = -currentVisible * canvasRect.rect.width;
+		endPos = startPos + canvasRect.rect.width;
+		doMove = true;
+		elapsedTime = 0f;
+		currentVisible--;
+		SetSwipeButtonsEnable ();
+	}
+
+	private void SetSwipeButtonsEnable() {
+		if (currentVisible == 0) {	//Showing the target stack
+			DisableLeftButton ();
+		} else {
+			EnableLeftButton ();
+		}
+
+		if (currentVisible == lastVisible) {
+			DisableRightButton ();
+		} else {
+			EnableRightButton ();
+		}
+	}
+
+	public void DisableLeftButton() {
+		PreviousButton.interactable = false;
+	}
+
+	public void DisableRightButton() {
+		NextButton.interactable = false;
+	}
+
+	public void EnableLeftButton() {
+		PreviousButton.interactable = true;
+	}
+
+	public void EnableRightButton() {
+		NextButton.interactable = true;
+	}
+
+}
